b25f6e0b67e3e40818703275315f5e7eaf853634
Anthony Nguyen <nguyenaanthony@gmail.com>
2021-09-17 19:14:36 -0500

    Combined two columns together and created a new column drop any duplicates from table and null data
    created function to remove any puncuation

--- main.py
+++ main.py
@@ -10,3 +10,31 @@ import string
 df = pd.read_csv('Fake.csv')
 
 print(df.head)
+
+df.drop_duplicates(inplace= True) #detect any duplicates
+df.shape
+
+df.isnull().sum()
+
+df.dropna(axis=0, inplace= True) #drop any data with missing fields
+df.shape
+
+#combines columns
+
+df['combined'] =df['title'] + ' '+ df['subject']
+print(df.head)
+
+nltk.download('stopwords')
+
+
+
+def process_text(text):
+   nopunc = [char for char in text if char not in string.punctuation]
+   nopunc = ''.join(nopunc)
+
+   clean_words = [word for word in nopunc.split() if word.lower() not in stopwords.words('english')]
+
+   return clean_words
+
+
+print(df['combined'].head().apply(process_text))
